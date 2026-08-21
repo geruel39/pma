3125c969b48896f80acce5b0f25a9c12f2e25170
Geruel Alcaraz <geruelalcaraz@gmail.com>
2026-08-21 15:21:23 +0800

    follow up

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -9,13 +9,16 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mb-2">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <div class="flex space-x-2">                   
-                        <a href="{{ route('projects.edit', $project->id) }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Edit this Project</a>
-                        <form action="{{ route('projects.destroy', $project->id) }}" method="POST" class="inline-block">
-                             @csrf
-                            @method('DELETE')
-                            <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Delete this Project</button>
-                        </form>
+                    <div class="flex space-x-2">
+                        <a href="{{ route('projects.index') }}" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Back to Project List</a>                   
+                        @if ($project->users->first()->pivot->user_id === auth()->user()->id && ($project->users->first()->pivot->role === 'owner' || $project->users->first()->pivot->role === 'manager')) 
+                            <a href="{{ route('projects.edit', $project->id) }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Edit this Project</a>
+                            <form action="{{ route('projects.destroy', $project->id) }}" method="POST" class="inline-block">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Delete this Project</button>
+                            </form>
+                        @endif
                     </div>
                 </div>
             </div>
